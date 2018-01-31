76ad8cd5f7e3630e7327da71497d82e0a6e142e7
gsalaun5 <gael.salaun@ac-rennes.fr>
2018-01-31 11:52:44 +0100

    Iteration 1

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,56 +3,73 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
+        for (item in items) {
+            if (!isSulfuras(item)) {
+                if (isBrie(item)) {
+                    updateQualityForBrie(item)
+                } else if (isBackstagePass(item)) {
+                    updateQualityForBackstagePass(item)
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
+                    updateQualityCommon(item)
                 }
             }
         }
     }
 
-}
+    private fun updateQualityCommon(item: Item) {
+        decreaseQuality(item)
+        decreaseSellIn(item)
+        if (itemExpired(item)) {
+            decreaseQuality(item)
+        }
+    }
 
+    private fun updateQualityForBrie(item: Item) {
+        increaseQuality(item)
+        decreaseSellIn(item)
+        if (itemExpired(item)) {
+            increaseQuality(item)
+        }
+    }
+
+    private fun itemExpired(item: Item) = item.sellIn < 0
+
+    private fun updateQualityForBackstagePass(item: Item) {
+        increaseQuality(item)
+        if (item.sellIn < 11) {
+            increaseQuality(item)
+        }
+        if (item.sellIn < 6) {
+            increaseQuality(item)
+        }
+        decreaseSellIn(item)
+        if (itemExpired(item)) {
+            item.quality = 0
+        }
+    }
+
+    private fun decreaseSellIn(item: Item) {
+        item.sellIn = item.sellIn - 1
+    }
+
+    private fun decreaseQuality(item: Item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1
+        }
+    }
+
+    private fun increaseQuality(item: Item) {
+        if (itemBelowMaxQuality(item)) {
+            item.quality = item.quality + 1
+        }
+    }
+
+    private fun itemBelowMaxQuality(item: Item) = item.quality < 50
+
+    private fun isSulfuras(item: Item) = item.name.equals("Sulfuras, Hand of Ragnaros")
+
+    private fun isBackstagePass(item: Item) = item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+
+    private fun isBrie(item: Item) = item.name.equals("Aged Brie")
+
+}
--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -9,10 +9,49 @@ class GildedRoseTest {
         val items = arrayOf<Item>(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
 
     }
 
+    @Test fun `New and legacy app should have same behaviour`() {
+        val itemsLegacy = arrayOf(Item("+5 Dexterity Vest", 10, 20), //
+                Item("Aged Brie", 2, 0), //
+                Item("Elixir of the Mongoose", 5, 7), //
+                Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                Item("Conjured Mana Cake", 3, 6))
+
+        val items = arrayOf(Item("+5 Dexterity Vest", 10, 20), //
+                Item("Aged Brie", 2, 0), //
+                Item("Elixir of the Mongoose", 5, 7), //
+                Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                // this conjured item does not work properly yet
+                Item("Conjured Mana Cake", 3, 6))
+
+        val appLegacy = GildedRoseLegacy(itemsLegacy)
+        val app = GildedRose(items)
+
+        for(i in 1..500){
+            appLegacy.updateQuality()
+            app.updateQuality()
+            for(i in 0..itemsLegacy.size-1){
+                for((item,itemLegacy) in items.zip(itemsLegacy)){
+                    assertEquals(item,itemLegacy)
+                }
+
+            }
+        }
+
+
+    }
 
 }
 
