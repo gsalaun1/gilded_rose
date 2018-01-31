0d02b033de584ccf847bb18584982bbfd4668085
gsalaun5 <gael.salaun@ac-rennes.fr>
2018-01-31 09:13:09 +0100

    Init

---
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -0,0 +1,20 @@
+package com.gildedrose
+
+import org.junit.Assert.*
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class GildedRoseTest {
+
+    @Test fun foo() {
+        val items = arrayOf<Item>(Item("foo", 0, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("fixme", app.items[0].name)
+
+    }
+
+
+}
+
+
